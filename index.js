572c7823d12369864ba2bf54e1ccc5f313b3b848
Jolene Kearse <pharaohnutz@gmail.com>
2022-10-20 13:36:02 -0400

    added btn click event grabbing text

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 import { tweetsData } from "./data.js";
+
+const tweetInput = document.getElementById("tweet-input");
+const tweetBtn = document.getElementById("tweet-btn");
+
+tweetBtn.addEventListener("click", function () {
+  console.log(tweetInput.value);
+});
