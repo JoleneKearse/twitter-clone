63d6363ddd85aa3537f5bd868fd90680b5436048
Jolene Kearse <pharaohnutz@gmail.com>
2022-10-20 14:46:11 -0400

    created function to build html for feed

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 import { tweetsData } from "./data.js";
-
 const tweetInput = document.getElementById("tweet-input");
 const tweetBtn = document.getElementById("tweet-btn");
 
+// log out new tweet
 tweetBtn.addEventListener("click", function () {
   console.log(tweetInput.value);
 });
+
+// display feed
+function getFeedHtml() {
+  let feedHtml = ``;
+  tweetsData.forEach(function (tweet) {
+    feedHtml += `
+      <div class="tweet">
+        <div class="tweet-inner">
+            <img src="${tweet.profilePic}" class="profile-pic">
+            <div>
+                <p class="handle">${tweet.handle}</p>
+                <p class="tweet-text">${tweet.tweetText}</p>
+                <div class="tweet-details">
+                    <span class="tweet-detail">
+                        ${tweet.replies.length}
+                    </span>
+                    <span class="tweet-detail">
+                        ${tweet.likes}
+                    </span>
+                    <span class="tweet-detail">
+                        ${tweet.retweets}
+                    </span>
+                </div>   
+            </div>            
+        </div>
+    </div>`;
+  });
+  return feedHtml;
+}
+
+getFeedHtml();
