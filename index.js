4e6bd1318edc9374923878a4db05e0a9f9fbb0f7
Jolene Kearse <pharaohnutz@gmail.com>
2022-10-21 16:38:55 -0400

    prevented empty tweets & cleared tweetInput value

--- index.js
+++ index.js
@@ -53,18 +53,21 @@ function handleReplyClick(replyId) {
 
 // new tweets
 function handleTweetBtnClick() {
-  tweetsData.unshift({
-    handle: `@Scrimba`,
-    profilePic: `./images/scrimbalogo.png`,
-    likes: 0,
-    retweets: 0,
-    tweetText: `${tweetInput.value}`,
-    replies: [],
-    isLiked: false,
-    isRetweeted: false,
-    uuid: `${uuidv4()}`,
-  });
-  render();
+  if (tweetInput.value) {
+    tweetsData.unshift({
+      handle: `@Scrimba`,
+      profilePic: `./images/scrimbalogo.png`,
+      likes: 0,
+      retweets: 0,
+      tweetText: `${tweetInput.value}`,
+      replies: [],
+      isLiked: false,
+      isRetweeted: false,
+      uuid: `${uuidv4()}`,
+    });
+    render();
+    tweetInput.value = "";
+  }
 }
 
 // build html for feed
